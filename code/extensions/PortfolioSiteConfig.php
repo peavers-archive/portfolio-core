c156c802af72c8838bfe32d7b94536c3b2f205bc
Chris Turner <chris.turner@minedu.govt.nz>
2015-05-24 14:52:02 -0400

    Updated requirements.

--- code/extensions/PortfolioSiteConfig.php
+++ code/extensions/PortfolioSiteConfig.php
@@ -20,13 +20,12 @@ class PortfolioSiteConfig extends DataExtension
     public function updateCMSFields(FieldList $fields)
     {
         $fields->addFieldsToTab('Root.Main', array(
-            UploadField::create("ProfileImage", "Icon/Image of product"),
+            UploadField::create("ProfileImage", "Profile image/logo"),
         ));
 
         $fields->addFieldsToTab('Root.FooterLinks', array(
             GridField::create('PortfolioFooterLinks', 'Links in the footer', $this->owner->PortfolioFooterLinks(), GridFieldConfig_RecordEditor::create()),
             TextField::create('BottomLine', 'Bottom line of the footer'),
         ));
-
     }
 }
